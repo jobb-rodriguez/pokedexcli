559a5474c5c1546a33ae6e5859da079fa7c30a22
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2025-01-07 21:19:00 +0800

    Bugfix: Return strings of slices

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
   "fmt"
 )
 
+func cleanInput(text string) []string {
+  // Split the input into words based on white space
+  // Lowercase the input
+  // Trim any leading or trailing white space
+  return []string{}
+}
+
 func main() {
   fmt.Println("Hello World!");
 }
