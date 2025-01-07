c4eaf41cabffb2b279105d8ca5b00c1c7cfd118a
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2025-01-07 22:30:26 +0800

    Update: Implement cleanInput()

--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import (
   "fmt"
+  "strings"
+  "errors"
 )
 
-func cleanInput(text string) []string {
-  // Split the input into words based on white space
-  // Lowercase the input
-  // Trim any leading or trailing white space
-  return []string{}
+func cleanInput(text string) ([]string, error) {
+  text = strings.Trim(text, " ")
+  words := strings.Split(text, " ")
+
+  for i, _  := range words {
+    words[i] = strings.ToLower(words[i])
+    words[i] = strings.Trim(words[i], " ")
+
+    if len(words[i]) == 0 ||  strings.Contains(words[i], " ") {
+      err := errors.New("Empty string")
+      return []string{}, err
+    }
+  }
+  return words, nil
 }
 
 func main() {
-  fmt.Println("Hello World!");
+  text := "  Hey  there  "
+  cleanedText, err := cleanInput(text);
+
+  if  err != nil {
+    fmt.Printf("Error: %v\n", err)
+    return
+  }
+  
+  for _, w := range cleanedText {
+    fmt.Println(w, len(w))
+  }
 }
